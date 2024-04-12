5738c16aa3b2bd687ce381688c8384ab4d7696ed
Jordan <126593883+jccox-dev@users.noreply.github.com>
2024-04-12 09:02:30 -0500

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -11,15 +11,15 @@ set +x
 echo 'The following command extracts the value of the <name/> element'
 echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
 set -x
-NAME=`mvn  help:evaluate -Dexpression=project.name -q -DforceStdout`
-NAME='my-app'
+NAME=mvn help:evaluate -Dexpression=project.name -q -DforceStdout`
+
 set +x
 
 echo 'The following command behaves similarly to the previous one but'
 echo 'extracts the value of the <version/> element within <project/> instead.'
 set -x
 VERSION=`mvn help:evaluate -Dexpression=project.version -q -DforceStdout`
-VERSION='1.0-SNAPSHOT'
+
 set +x
 
 echo 'The following command runs and outputs the execution of your Java'
